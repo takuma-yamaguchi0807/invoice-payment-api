108f0a01f15bea6da19c4702c1bc073116523712
takuma-yamaguchi <yamataku0807@gmail.com>
2025-11-16 12:51:12 +0900

    javaプロジェクトの作成

---
+++ src/main/java/com/example/invoicepaymentapi/InvoicePaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoicepaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicePaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicePaymentApiApplication.class, args);
+    }
+}
+
